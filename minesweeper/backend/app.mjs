843989686ed23be3fb12690c9f7da54b378f512d
3104K <3104k.african@gmail.com>
2023-02-05 20:40:50 +0900

    modify : サーバーサイド - 文字コードの設定

--- minesweeper/backend/app.mjs
+++ minesweeper/backend/app.mjs
@@ -11,6 +11,9 @@ import * as http from "http";
 const server = http.createServer(function(req, res){
 
     let dispMessage = "No Message!";
+
+    // 文字化けを防ぐため、content-typeを設定
+    // res.writeHead(200,{'content-type': 'text/html; charset=UTF-8'});
     if(req.url === "/hello"){
         // http://localhost:8080/helloのとき画面に表示
         dispMessage = "Hello!";
@@ -18,9 +21,21 @@ const server = http.createServer(function(req, res){
         // http://localhost:8080/byeのとき画面に表示
         dispMessage = "Bye!";
     }
-    console.log("Hello");
+    console.log(dispMessage);
     // responseを返す際はresのメソッドを使用する
-    res.end(dispMessage);
+    // responseとHTML両方に文字コードの情報を詰めるのが一般的
+    res.end(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>テストテスト</h1>
+    </body>
+    </html>`);
 });
 
 // 引数に指定したポート番号でサーバーが立ち上がるようになる

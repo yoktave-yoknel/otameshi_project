6ae2cdeb82bf1eec0cc923d712e3b7bc6d382342
3104K <3104k.african@gmail.com>
2023-02-05 20:03:45 +0900

    modify : サーバーサイド - コメントの修正

--- minesweeper/backend/app.mjs
+++ minesweeper/backend/app.mjs
@@ -1,6 +1,7 @@
 import * as http from "http";
 
 /**
+ * expressは使用していない
  * ターミナルで"node app.mjsのファイルパス"を入力する事でサーバーが起動
  * その後、http://localhost:8080をブラウザで入力すると、サーバーにリクエスト情報が送信される
  * @param {*} req : URL叩いたときのリクエスト情報
@@ -8,7 +9,9 @@ import * as http from "http";
  */
 const server = http.createServer(function(req, res){
     console.log("request：urlだよー",req.url);
-    res.end('response：エンドだよー');
+
+    // responseを返す際はresのメソッドを使用する
+    res.end('response test');
 });
 
 // 引数に指定したポート番号でサーバーが立ち上がるようになる

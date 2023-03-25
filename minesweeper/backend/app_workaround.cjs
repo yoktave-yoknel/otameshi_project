58fb32fdfbe9a4dabcea18f39163bb631dfd4d6d
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2023-03-25 16:47:53 +0900

    DynamoDBへの書き込み可能版
    クリア時の情報をDynamoDBへ書き込む実装を行った。

--- minesweeper/backend/app_workaround.cjs
+++ minesweeper/backend/app_workaround.cjs
@@ -2,26 +2,65 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended:true}));
 
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, PutCommand } = require('@aws-sdk/lib-dynamodb');
+const REGION = "ap-northeast-1"; // EC2およびDynamoDBのリージョンを指定
+
+const marshallOptions = {
+    // Whether to automatically convert empty strings, blobs, and sets to `null`.
+    convertEmptyValues: false, // false, by default.
+    // Whether to remove undefined values while marshalling.
+    removeUndefinedValues: false, // false, by default.
+    // Whether to convert typeof object to map attribute.
+    convertClassInstanceToMap: false, // false, by default.
+};
+const unmarshallOptions = {
+    // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+    wrapNumbers: false, // false, by default.
+};
+const translateConfig = { marshallOptions, unmarshallOptions };
+const ddbClient = new DynamoDBClient({ region: REGION });
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient, translateConfig);
+
+app.post('/minesweeper', (req, res) => {
+    // MEMO: ローカル確認の際には以下を有効にする
+    //res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // DBへのレコード追加
+    var nowDate = new Date();
+    var params = {
+        TableName: 'scores',
+        Item: {
+            'date': nowDate.toLocaleString('ja-JP'),
+            'cleartime': parseInt(req.body.cleartime, 10),
+        }
+    };
+    const run = async () => {
+        try {
+            const data = await ddbDocClient.send(new PutCommand(params));
+            console.log("Success - item added or updated", data);
+        } catch (err) {
+            console.log("Error", err);
+        }
+        return;
+    };
+    run();
+
+    // TODO: DBからランキング取得
+
+    console.log(req.body.cleartime);
+    if(req.body.cleartime <= 60){
+        res.send('MARVELOUS!!!');
+    } else {
+        res.send('GOOD JOB.');
+    }
+});
+
 // GETメソッドについては静的アセットを公開
 app.use('/minesweeper', express.static('client'));
 app.get('/minesweeper', (req, res) => {
 });
 
-app.get('/', (req, res) => {
-    res.send("access: root");
-});
-
-
-// POSTメソッドはクリア時にクライアントから呼ばれる
-app.post('/minesweeper', (req, res) => {
-    console.log(req.body.cleartime);
-    if(req.body.cleartime <= 60){
-        res.send("MARVELOUS!!!");
-    } else {
-        res.send("GOOD JOB.");
-    }
-});
-
 app.listen(8080, () => {
-    console.log("server running...");
+    console.log('server running...');
 });
